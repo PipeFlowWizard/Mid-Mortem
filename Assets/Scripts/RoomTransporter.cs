a642092ebfc66b4a17bf82870526d71bc48bcee8
unknown <nathanziri@hotmail.com>
2021-10-24 23:53:30 -0400

    imported room prefab

---
+++ Assets/Scripts/RoomTransporter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransporter : MonoBehaviour
+{
+    public RoomTransporter[] AdjecentRooms= new RoomTransporter[4];
+    public Transform[] AdjacentDoors = new Transform[4];
+    
+    
+    public void teleportPlayer(GameObject g, int i)
+    {
+        Debug.Log("Hit confirmed");
+        int goTo = (i + 2) % 4;
+        switch (i)
+        {
+            case 0:
+                Debug.Log("Move North");
+                g.transform.position = (AdjecentRooms[i].AdjacentDoors[goTo].position + new Vector3(0, 0, 3));
+                break;
+            case 1:
+                Debug.Log("Move East");
+                g.transform.position = (AdjecentRooms[i].AdjacentDoors[goTo].position + new Vector3(-3, 0, 0));
+                break;
+            case 2:
+                Debug.Log("Move South");
+                g.transform.position = (AdjecentRooms[i].AdjacentDoors[goTo].position + new Vector3(0, 0, -3));
+                break;
+            case 3:
+                Debug.Log("Move West");
+                g.transform.position = (AdjecentRooms[i].AdjacentDoors[goTo].position + new Vector3(3, 0, 0));
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public int roomNum;
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        transform.parent.GetComponent<RoomTransporter>().teleportPlayer(other.gameObject , roomNum);
+    }
+}
