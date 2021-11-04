848375915a84ff580d528e076047d8aa5d98da68
JustinCBoutin <49909521+JustinCBoutin@users.noreply.github.com>
2021-11-03 20:40:07 -0400

    Pause UI Video Playing

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,24 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject player;
     public GameObject input;
+    [SerializeField] GameObject videoPlayer;
+    private VideoPlayer video;
 
     void Start()
     {
         gameObject.SetActive(false);
+        video = videoPlayer.GetComponent<VideoPlayer>();
+    }
+
+    private void Awake()
+    {
+        // Doesn't Show UI Elements of the Pause Menu
+        gameObject.transform.GetChild(4).gameObject.SetActive(false);
     }
 
     public void pauseGame()
     {
+        
+        print(video.frame);
+        // TODO: Only Show UI When Video Stopped Playing
+        if (video.frame > 100)
+        {
+            showInfo();
+        }
         player.GetComponent<PlayerController>().enabled ^= true;
         input.SetActive(!input.activeSelf);
         Time.timeScale = (Time.timeScale == 1.0f ? 0.0f : 1.0f);
         gameObject.SetActive(!gameObject.activeSelf);
         
+
+    }
+
+    void showInfo()
+    {
+        print("In Coroutine");
+        gameObject.transform.GetChild(4).gameObject.SetActive(true);
+        print("Done Coroutine");
     }
 
     public void options()
