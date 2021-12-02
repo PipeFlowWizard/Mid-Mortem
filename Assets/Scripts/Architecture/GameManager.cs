2fb5f0eaa0cb708af8a925f5cb8997999b865d99
PipeFlowWizard <elijahmon96@gmail.com>
2021-12-01 22:15:41 -0500

    update

--- Assets/Scripts/Architecture/GameManager.cs
+++ Assets/Scripts/Architecture/GameManager.cs
@@ -13,7 +13,9 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        //Load screen
+        // Main menu
+        // Create level
         initializeGame();
             
     }
