b418e1e7112f3bc6d20998ae10764ce815bd9b54
Robert Michaud <robert.cn.michaud@hotmail.com>
2021-12-05 17:39:11 -0500

    main menu merge and async loading

--- Assets/Scripts/Abilities/Spell.cs
+++ Assets/Scripts/Abilities/Spell.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Cinemachine;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,17 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private string playSceneName;
-    [SerializeField] private string optionSceneName;
+    [SerializeField] private int playSceneIndex;
+    [SerializeField] private string optionSceneIndex;
+    [SerializeField] private GameObject loading;
+    [SerializeField] private GameObject startButton;
 
     public void play()
     {
-        SceneManager.LoadScene(playSceneName, LoadSceneMode.Single);
+        // SceneManager.LoadScene(playSceneIndex, LoadSceneMode.Single);
+        loading.SetActive(true);
+        startButton.SetActive(false);
+        StartCoroutine(LoadYourAsyncScene());
     }
 
     public void options()
     {
-        SceneManager.LoadScene(optionSceneName, LoadSceneMode.Single);
+        SceneManager.LoadScene(optionSceneIndex, LoadSceneMode.Single);
     }
 
     public void quit()
@@ -24,5 +29,23 @@ public class MainMenu : MonoBehaviour
             UnityEditor.EditorApplication.isPlaying = false;
         #endif
             Application.Quit();
+    }
+    
+    private IEnumerator LoadYourAsyncScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(playSceneIndex);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log("Loading..");
+            yield return null;
+        }
+
     }
 }
